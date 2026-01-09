c41637ac9bf1fd0139b13bb94014fb52b40f61d9
Vasilis Kortsimelidis <vaskort@gmail.com>
2026-01-09 16:57:22 +0000

    Basic check if there is a yarn or npm project.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 use clap::Parser;
+use std::path::Path;
+
+#[derive(Debug)]
+enum LockFileType {
+    Yarn,
+    Npm,
+}
 
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Cli {
     package: String,
+
+    #[arg(short, long, default_value = ".")]
+    path: String,
+}
+
+fn check_lockfile(project_path: &str) -> Result<LockFileType, String> {
+    let yarn_exists = Path::new(project_path).join("yarn.lock").exists();
+    let npm_exists = Path::new("package-lock.json").exists();
+
+    match (yarn_exists, npm_exists) {
+        (true, _) => Ok(LockFileType::Yarn),
+        (_, true) => Ok(LockFileType::Npm),
+        _ => Err("No lockfile found".to_string()),
+    }
 }
 
 fn main() {
     let cli = Cli::parse();
     println!("Hello bro {}", cli.package);
+
+    let lockfile_type = match check_lockfile(&cli.path) {
+        Ok(lockfile_type) => lockfile_type,
+        Err(err) => panic!("{}", err),
+    };
+
+    println!("Found lockfile, {:?}", lockfile_type)
 }
