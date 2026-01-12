484ca1e6e11ebe9beda57fbfe23c871cd8698c4b
Vasilis Kortsimelidis <vaskort@gmail.com>
2026-01-12 16:40:45 +0000

    Parsin yarn lock file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use clap::Parser;
+use std::fs::read_to_string;
 use std::path::Path;
 
 #[derive(Debug)]
 enum LockFileType {
     Yarn,
     Npm,
+}
+
+impl LockFileType {
+    fn file_name(&self) -> &str {
+        match self {
+            LockFileType::Yarn => "yarn.lock",
+            LockFileType::Npm => "package-lock.json",
+        }
+    }
 }
 
 #[derive(Parser)]
@@ -18,7 +28,7 @@ struct Cli {
 
 fn check_lockfile(project_path: &str) -> Result<LockFileType, String> {
     let yarn_exists = Path::new(project_path).join("yarn.lock").exists();
-    let npm_exists = Path::new("package-lock.json").exists();
+    let npm_exists = Path::new(project_path).join("package-lock.json").exists();
 
     match (yarn_exists, npm_exists) {
         (true, _) => Ok(LockFileType::Yarn),
@@ -27,14 +37,28 @@ fn check_lockfile(project_path: &str) -> Result<LockFileType, String> {
     }
 }
 
+fn parse_lockfile(lock_file: &Path) -> Result<String, String> {
+    read_to_string(lock_file).map_err(|err| err.to_string())
+}
+
 fn main() {
     let cli = Cli::parse();
-    println!("Hello bro {}", cli.package);
+    dbg!(cli.package);
 
     let lockfile_type = match check_lockfile(&cli.path) {
         Ok(lockfile_type) => lockfile_type,
         Err(err) => panic!("{}", err),
     };
+
+    dbg!(&lockfile_type);
+
+    let lockfile_path = Path::new(&cli.path).join(lockfile_type.file_name());
+    let lockfile_content = match parse_lockfile(&lockfile_path) {
+        Ok(content) => content,
+        Err(err) => panic!("{}", err),
+    };
+
+    dbg!(&lockfile_content);
 
     println!("Found lockfile, {:?}", lockfile_type)
 }
